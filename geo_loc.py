26057c5b9ba8cb812f13112a09701b482648d9fa
korius668 <korius668@gmail.com>
2025-06-14 14:56:48 +0200

    cut sign pictures

--- geo_loc.py
+++ geo_loc.py
@@ -1,3 +1,4 @@
+import os
 import cv2 as cv
 from ultralytics import YOLO
 from IPython.display import Video
@@ -7,13 +8,26 @@ import seaborn as sns
 
 model = YOLO("models/fine_tuned_yolov8s.pt")
 
-cap = cv.VideoCapture("film.mp4")
+cap = cv.VideoCapture("vid1.mov")
 
 width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 fps = cap.get(cv.CAP_PROP_FPS)
 fourcc = cv.VideoWriter_fourcc(*"mp4v")  # lub 'XVID' dla AVI
 out = cv.VideoWriter("output_detected.mp4", fourcc, fps, (width, height))
+
+frame_count = 0
+object_id = 0
+
+output_dir = "detected_objects"
+classes = set(['different-traffic-sign', 'prohibition-sign', 'speed-limit-sign', 'warning-sign'])
+# Sprawdź czy folder istnieje, jeśli nie — stwórz
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+    print(f"📁 Folder '{output_dir}' został utworzony.")
+else:
+    print(f"✅ Folder '{output_dir}' już istnieje.")
+
 
 # 4. Pętla po klatkach
 while cap.isOpened():
@@ -23,12 +37,28 @@ while cap.isOpened():
 
     # 5. Detekcja
     results = model(frame)[0]  # YOLOv8 returns list; take the first
-
-    # 6. Rysowanie wykryć
     annotated_frame = results.plot()  # rysuje boxy, etykiety itd.
-
-    # 7. Zapis klatki
     out.write(annotated_frame)
+
+    if results.boxes is not None:
+        boxes = results.boxes
+        for box in boxes:
+            class_id = int(box.cls[0])
+            class_name = model.names[class_id]
+            if class_name in classes:
+                # Koordynaty bboxa
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                
+
+                # Wytnij obiekt
+                cropped = frame[y1:y2, x1:x2]
+
+                # Zapisz obrazek
+                filename =os.path.join("detected_objects", f"frame{frame_count:04d}_{class_name}_{object_id}.jpg")
+                cv.imwrite(filename, cropped)
+                object_id += 1
+            
+    frame_count += 1
 
 # 8. Zwolnienie zasobów
 cap.release()
